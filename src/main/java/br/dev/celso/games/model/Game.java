68fb92254af0996f08a4c62b5356dc5e2b93a081
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2024-10-16 14:29:54 -0300

    Entidade Game

---
+++ src/main/java/br/dev/celso/games/model/Game.java
@@ -0,0 +1,12 @@
+package br.dev.celso.games.model;
+
+import java.time.LocalDate;
+
+public class Game {
+
+    private Long gameId;
+    private String title;
+    private String value;
+    private LocalDate releaseDate;
+
+}
